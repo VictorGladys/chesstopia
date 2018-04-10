86703e762a79625ed7ee42e6b86008c41d92e489
Victor Gladys <victorgladys@gmail.com>
2018-04-10 21:57:38 +0200

    added Game class for initialization of window

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+
+class Game:
+    def __init__(self, size):
+        pygame.init()
+        self.size = size
+        self.screen = pygame.display.set_mode(size)
+        pygame.display.set_caption("Chesstopia")
+        self.clock = pygame.time.Clock()
+        self.isPlaying = True
+        self.colors = {"w": (255, 255, 255), "r": (255, 0, 0)}
+
+    def start(self):
+        while(self.isPlaying):
+            for event in pygame.event.get():    #a user gave input
+                if event.type == pygame.QUIT:   # If user clicked close
+                    self.isPlaying = False      # Flag that we are done so we exit this loop
+            self.screen.fill(self.colors["w"])
+
+            # The you can draw different shapes and lines or add text to your background stage.
+            pygame.draw.rect(self.screen, self.colors["r"], [55, 200, 100, 70],0)
+
+    def quit(self):
+        pygame.quit()
+
+def main():
+    game = Game((700,500))
+    game.start()
+    game.quit()
+
+if __name__ == "__main__":
+    main()
