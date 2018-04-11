bd83bcbf4109de7b80c5dc0b1845b582617842d3
Victor Gladys <victorgladys@gmail.com>
2018-04-11 02:42:19 +0200

    added 'find_neighbors' function

--- gamelogic/board_graph.py
+++ gamelogic/board_graph.py
@@ -9,7 +9,7 @@ modes = Enum(["normal", "donut", "cube"])
 
 class Board:
     """
-    Size should be given in a tuple of (m_1, ...m_n)
+    Size should be given in a tuple of (m_1, ..., m_n)
     Mode should be an enum defined above, such as mode.normal or mode.donut
     """
     def __init__(self, dim, mode):
@@ -18,19 +18,36 @@ class Board:
         self.id = 0
         self.dict = {}
         self.fill_board((), dim)
+        self.add_connections()
 
     def __repr__(self):
-        return json.dumps(self.dict)
+        return str(self.dict)
 
     def fill_board(self, idxs, dim):
+        """
+        Adds nodes to dict for every coordinate-permutation in a tuple of form (m_1, ..., m_n)
+        """
         if not dim:
-            self.dict.update({self.id: (idxs, {})})
+            self.dict.update({idxs: []})
             self.id += 1
             return
         for i in range(dim[0]):
             self.fill_board((i, *idxs), dim[1:])
 
+    def add_connections(self):
+        for k, v in self.dict.items():
+            neighbors = []
+            for i in range(len(k)):
+                tmp = list(k)
+                tmp[i] += 1
+                if tmp[i] <= len(k) + 1:
+                    neighbors.append(tmp)
+                tmp = list(k)
+                tmp[i] -= 1
+                if tmp[i] >= 0:
+                    neighbors.append(tmp)
+            self.dict[k] = self.dict[k] + neighbors
 
 if __name__ == "__main__":
-    b = Board((4,4,4), modes.normal)
+    b = Board((4,4), modes.normal)
     print(b)
